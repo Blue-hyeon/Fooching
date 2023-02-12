8db3ce5e937fb5800c211a335c996e7ff0abf798
Leesungsup <tmdtjq0116@naver.com>
2023-02-13 02:31:07 +0900

    MatchingActivity 트레이너 정보

--- app/src/main/java/com/example/healthcare/matching/MatchingActivity.java
+++ app/src/main/java/com/example/healthcare/matching/MatchingActivity.java
@@ -1,22 +1,27 @@
 package com.example.healthcare.matching;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.healthcare.R;
 import com.example.healthcare.chat.ChatFragment;
 import com.example.healthcare.chat.MessageActivity;
 import com.example.healthcare.model.ChatModel;
+import com.example.healthcare.model.UserModel;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -35,6 +40,11 @@ public class MatchingActivity extends AppCompatActivity {
     private String chatRoomUid;
     private RecyclerView recyclerView;
     TextView textbutton;
+    private TextView trainername;
+    private TextView trainerlocation;
+    private TextView trainerinfo;
+    private ImageView trainerimage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +52,11 @@ public class MatchingActivity extends AppCompatActivity {
         uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         destinationUid = getIntent().getStringExtra("destinationUid");
         textbutton = (TextView) findViewById(R.id.matchingBtn);
+
+        trainername = (TextView) findViewById(R.id.matching_cv_name);
+        trainerlocation = (TextView) findViewById(R.id.matching_cv_location);
+        trainerinfo=(TextView) findViewById(R.id.matching_cv_info);
+        trainerimage=(ImageView) findViewById(R.id.matching_cv_profile);
         textbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,6 +79,36 @@ public class MatchingActivity extends AppCompatActivity {
             }
         });
         checkChatRoom();
+        setCardView();
+    }
+    void setCardView(){
+        ArrayList<UserModel> userModels = new ArrayList<>();
+        FirebaseDatabase.getInstance().getReference().child("users").orderByChild("uid").equalTo(destinationUid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot :dataSnapshot.getChildren()){
+
+                    UserModel userModel = snapshot.getValue(UserModel.class);
+
+                    userModels.add(userModel);
+                }
+                Log.e("333333333",userModels.get(0).userName);
+                if(userModels.get(0).comment!=null) {
+                    trainerinfo.setText(userModels.get(0).comment);
+                }
+                Log.e("333333333",userModels.get(0).profileImageUrl);
+                trainername.setText(userModels.get(0).userName);
+                Glide.with(getApplicationContext())
+                        .load(userModels.get(0).profileImageUrl)
+                        .into(trainerimage);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
     void checkChatRoom() {
 
